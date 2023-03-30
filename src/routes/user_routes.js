7d35aa653daf4bd60fe80f3d019fc53255ad8149
AndroidLearners2023 <and044604@gmail.com>
2023-03-30 16:12:17 +0530

    few files changed

--- src/routes/user_routes.js
+++ src/routes/user_routes.js
@@ -2,8 +2,8 @@ const express = require("express");
 const { signUp, signIn } = require("../controllers/userControllers");
 const userRouter = express.Router();
 
-userRouter.post("/signup", signUp);
+userRouter.post("/sign_up", signUp);
 
-userRouter.post("/signin", signIn);
+userRouter.post("/sign_in", signIn);
 
 module.exports = userRouter;
